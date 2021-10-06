8c1215461fc312452da023526cf48999f44de1c7
tsuruo <tsuru.dev@gmail.com>
2021-10-06 13:42:22 +0900

    ProductのIdentifier指定で取得する処理を追加

--- flutter_revenuecat_practice/lib/controller/purchase_state.dart
+++ flutter_revenuecat_practice/lib/controller/purchase_state.dart
@@ -13,4 +13,11 @@ class PurchaseState with _$PurchaseState {
   late final Offering? currentOffering = offerings?.current;
   late final List<Product>? products =
       offerings?.current?.availablePackages.map((p) => p.product).toList();
+
+  late final Product? product = products?.firstWhereOrNull(
+    (e) => e.identifier == _identifier,
+  );
 }
+
+// TODO(tsuruoka): 本来は`bundle ID`を取得する実装をするとベター
+const _identifier = 'com.tsuruoka.flutterRevenueCatPractice.monthly';
